bfd55f08049d1d91fe4e09d633ba1ef0487ae64e
Amit Kandar <kandaramit2001@gmail.com>
2024-02-10 10:02:35 +0530

    Added React router dom and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from './routes/Home';
+import AboutUs from './routes/AboutUs';
+import ContactUs from './routes/ContactUs';
+import Services from './routes/Services';
+import Testimonials from "./routes/Testimonials";
+
 function App() {
   return (
-    <>
-
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/testimonials" element={<Testimonials />} />
+      </Routes>
+    </Router>
   );
 }
 
