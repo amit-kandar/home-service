f31343e4ff117b70e521ddfebe06058bcb23963a
Amit Kandar <kandaramit2001@gmail.com>
2024-02-16 18:29:44 +0530

    Import the about image

--- src/config/constants.js
+++ src/config/constants.js
@@ -10,3 +10,4 @@ export const SERVICE_BG_IMG = require('../assets/service-bg-img.jpg');
 export const TESTIMONIALS_BG_IMG = require('../assets/design-img.png');
 export const TEST_BANNER_IMG = require('../assets/test-banner-img.png');
 export const CONTACT_IMG = require('../assets/contact-img.png');
+export const ABOUT_IMG = require('../assets/about-banner-img.png');
